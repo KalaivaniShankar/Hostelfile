a343774dc741dc076e4b3ca81a318f0eea738c82
KalaivaniShankar <89274886+KalaivaniShankar@users.noreply.github.com>
2021-08-21 01:56:49 +0530

    initial submission

---
+++ Hostel.java
@@ -0,0 +1,12 @@
+package org.college;
+
+public class Hostel {
+public void hostelName() {
+System.out.println("HOSTELNAME=ravi");	
+}
+	public static void main(String[] args) {
+		Hostel hh=new Hostel();
+		hh.hostelName();
+	}
+
+}
